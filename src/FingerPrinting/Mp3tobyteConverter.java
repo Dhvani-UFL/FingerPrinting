1e0baa91906015c8acb378b7173cefc8d1443bc4
jsrikrish <jsrikrish@gmail.com>
2014-12-10 17:11:08 -0500

    made some additions

--- src/FingerPrinting/Mp3tobyteConverter.java
+++ src/FingerPrinting/Mp3tobyteConverter.java
@@ -5,11 +5,18 @@ import java.io.InputStream;
 
 public class Mp3tobyteConverter 
 {
-	public byte[] inputStreamToByteArray(InputStream inStream) throws IOException {
+	
+	// This method is called to convert an input stream to a byte array.
+	// This byte array is used in the FingerPrintGenerator class. 
+	// The actual finger printing calculations are done on the byte array.
+	
+	public byte[] inputStreamToByteArray(InputStream inStream) throws IOException 
+	{
 	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
 	    byte[] buffer = new byte[8192];
 	    int bytesRead;
-	    while ((bytesRead = inStream.read(buffer)) > 0) {
+	    while ((bytesRead = inStream.read(buffer)) > 0) 
+	    {
 	        baos.write(buffer, 0, bytesRead);
 	    } 
 	    return baos.toByteArray();
